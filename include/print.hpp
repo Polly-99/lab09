3b3dde205ecccb66526812e5fa459bdeb7f7d990
Polly-99 <prasnach@mail.ru>
2017-10-25 19:10:33 +0300

    added doxygen.conf

--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,17 @@
 #include <fstream>
 #include <iostream>
 
+//!Функция вывода строки в поток вывода
+/*!
+\param text аргумент типа std::string
+\param out аргумент типа std::ostream
+*/
 void print(const std::string& text, std::ostream& out = std::cout);
+
+//! Функция для вывода строки в файловый поток
+/*!
+\param text аргумент типа std::string
+\param out аргумент типа std::ofstream
+*/
+
 void print(const std::string& text, std::ofstream& out);
